fb7bf2a7f2fec02757d3c496d32e612a2790d196
Darko <d.siracevski@gmail.com>
2022-07-26 18:40:46 +0200

    Reworked table into grid table, extracted some components

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="mk">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EVNoPower</title>
+    @vite('resources/css/app.css')
+</head>
+
+    <body>
+
+        {{$slot}}
+
+    </body>
+
+</html>
