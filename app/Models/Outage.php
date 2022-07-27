d377e3362095cd19fb09827f06104b438bfdbe8a
Darko <d.siracevski@gmail.com>
2022-07-27 14:28:04 +0200

    cleaned up the code

--- app/Models/Outage.php
+++ app/Models/Outage.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+//use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class Outage extends Model
 {
@@ -13,5 +15,31 @@ class Outage extends Model
 
     protected $dates = ['start', 'end', 'created_at', 'updated_at'];
 
+    protected $date;
+
+
+    public function scopeFilter($query)
+    {
+        $outage = self::latest('end')->first();
+
+        if ($outage->end <= now()) {
+            $this->date = Carbon::today()->endOfDay();
+        } else {
+            $this->date = Carbon::tomorrow()->endOfDay();
+        }
+
+        if (request('date')) {
+            $endDate = (request()->date) ? Carbon::parse(request()->date)->endOfDay() : $this->date;
+
+            $startDate = Carbon::parse($endDate)->startOfDay();
+
+            return $query->whereBetween('start', [$startDate, $endDate]);
+        }
+
+        if (request('area')) {
+            return $query->where('area', 'like', '%'.request()->area.'%');
+        }
+
+    }
 
 }
