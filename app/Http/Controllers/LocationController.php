5149011717ec78b8dcc907b6b9e4a5b96059f736
Darko <d.siracevski@gmail.com>
2022-07-28 13:16:14 +0200

    fixed location sync function, now it compares locations from outages and existing locations from... wait for it... LOCATIONS table, returns the difference and adds them to db

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -9,9 +9,13 @@ class LocationController extends Controller
 {
     public function syncLocations()
     {
-        $locations = Outage::locations()->pluck('location');
+        $locations = Location::all()->pluck('name')->toArray();
 
-        foreach ($locations as $location) {
+        $outageLocations = Outage::locations()->pluck('location')->unique();
+
+        $outageLocations = array_diff($outageLocations, $locations);
+
+        foreach ($outageLocations as $location) {
             Location::create(['name' => $location]);
         }
     }
