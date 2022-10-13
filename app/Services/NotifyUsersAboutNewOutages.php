ada3ef27225b90a80f79d117e5ac16d96942460a
Stojancho Sirachevski <stojancho.sirachevski@riskhub.co.uk>
2022-10-13 18:33:22 +0100

    Refactor

--- app/Services/NotifyUsersAboutNewOutages.php
+++ app/Services/NotifyUsersAboutNewOutages.php
@@ -17,10 +17,10 @@ class NotifyUsersAboutNewOutages
         // Get all users that have subscribed for notifications
         $usersWithLocations = User::has('locations')->with('locations')->get();
 
-        if ($plannedOutages !== null) {
-            foreach ($usersWithLocations as $user) {
-                $uLocations = $user->locations()->pluck('name');
-            }
+        if (! $plannedOutages) return;
+
+        foreach ($usersWithLocations as $user) {
+            $uLocations = $user->locations()->pluck('name');
 
             // Checks for and returns all planned outages for the cities the user has subscribed for
             $plannedOutages = array_intersect_key($plannedOutages->toArray(), $uLocations->toArray());
@@ -31,13 +31,7 @@ class NotifyUsersAboutNewOutages
                 ->whereDoesntHave('users')
                 ->get();
 
-        }
-// TODO only sending outages to one user
-        foreach ($usersWithLocations as $user) {
             Mail::to($user)->send(new PlannedOutages($plannedOutages, $user));
         }
-//        else {
-//            Log::info("No new mail to send");
-//        }
     }
 }
